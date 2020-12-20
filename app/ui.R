6e6d968ed1b5ff312a839fc254dc3d3ee0b1e007
James Baird <j.baird94@gmail.com>
2020-12-20 12:51:23 +0000

    Add SEO

--- app/ui.R
+++ app/ui.R
@@ -1,20 +1,34 @@
 library(shinythemes)
 
-navbarPage(fluid = TRUE, theme=shinytheme("flatly"), collapsible = TRUE,
-  "AmbStat",
-  tabPanel("Cardiac Arrest", cardiacArrestUi("cardiacArrest")),
-  tabPanel("STEMI", stemiUi("stemi")),
-  tabPanel("Call Outcomes", callOutcomesUi("callOutcomes")),
-  tabPanel("Response Times", responseTimesUi("responseTimes")),
-  tabPanel("Activity", activityUi("activity")),
-  footer = tagList(
-    wellPanel(
-      p(
-        "All data sourced from ",
-        a(target="_blank", href="https://www.england.nhs.uk/statistics/statistical-work-areas/ambulance-quality-indicators/", "NHS England - Ambulance Quality Indicators")
-      ),
-      p("Created by James Baird - Paramedic"),
-      a(href="https://github.com/bairdj/ambstat", target="_blank", "Github")
-    )
+analyticsTag <- function() {
+  id <- Sys.getenv("GOOGLE_ANALYTICS")
+  if (nchar(id) > 0) {
+    return(htmlTemplate('analytics.html', ID = id, document_ = F))
+  } else {
+    return(tagList())
+  }
+}
+
+tagList(
+  tags$head(analyticsTag()),
+  tags$head(includeHTML('meta.html')),
+  navbarPage(fluid = TRUE, theme=shinytheme("flatly"), collapsible = TRUE,
+             windowTitle = "AmbStat: English ambulance statistics",
+             "AmbStat",
+             tabPanel("Cardiac Arrest", cardiacArrestUi("cardiacArrest")),
+             tabPanel("STEMI", stemiUi("stemi")),
+             tabPanel("Call Outcomes", callOutcomesUi("callOutcomes")),
+             tabPanel("Response Times", responseTimesUi("responseTimes")),
+             tabPanel("Activity", activityUi("activity")),
+             footer = tagList(
+               wellPanel(
+                 p(
+                   "All data sourced from ",
+                   a(target="_blank", href="https://www.england.nhs.uk/statistics/statistical-work-areas/ambulance-quality-indicators/", "NHS England - Ambulance Quality Indicators")
+                 ),
+                 p("Created by James Baird - Paramedic"),
+                 a(href="https://github.com/bairdj/ambstat", target="_blank", "Github")
+               )
+             )
   )
 )
